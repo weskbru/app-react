53739f3f95caf61986101ba594de668ce3f65fab
wesley bruno <weskbruno@gmail.com>
2022-10-25 00:38:51 -0300

    criando o corpo da aplicação

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+import {HiOutlineSearch} from 'react-icons/hi'
+
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>ola mundo</h1>
+    <div className="container">
+      <h1 className="title">Buscado Cep</h1>
+      <div className="containe-input">
+        <input type="text" placeholder='Digite seu Cep' />
+        <button className='button-input'> 
+          <HiOutlineSearch size={25} color="#fff" />
+        </button>
+      </div>
+      <main className='main'>
+        <h2>Cep: 123901204</h2>
+        <span>Reua teste</span>
+        <span>complemento</span>
+        <span>vila rosa</span>
+        <span>campo barrp</span>
+      </main>
     </div>
-  )
+  );
+
+
 }
 
 export default App
